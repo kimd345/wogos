a2e70e1b060a5d4178419ac7e676bd62edada213
Austin Burger <aburger91@gmail.com>
2020-08-31 16:11:26 -0500

    add seed data to db to begin routes

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from starter_app.models import User
+from starter_app.models import User, Order, Game, Review, Genre, Feature
 from starter_app import app, db
 from dotenv import load_dotenv
 load_dotenv()
@@ -21,11 +21,47 @@ with app.app_context():
     alissa = User(username='Alissa', email='alissa@aa.io',
                   hashed_password='auhgdfiaufhggasrgargarg')
 
+    order1 = Order(user_id=1, game_id=1, price_paid=19.99)
+    order2 = Order(user_id=2, game_id=1, price_paid=19.99)
+    order3 = Order(user_id=3, game_id=2, price_paid=59.99)
+
+    game1 = Game(title="blahs adventure", price=19.99, description="this game is trash", requirements='a super dope pc')  # noqa
+    game2 = Game(title="blash second adventure", price=59.99, description="the devs actually game through on this one", requirements='1080 TI')  # noqa
+
+    review1 = Review(user_id=1, game_id=1, title="MUST BUY", body="This is a must have game this summer", star_rating=4)  # noqa
+    review2 = Review(user_id=3, game_id=2, title="meh its alright", body="i think the graphics could have been better", star_rating=2)  # noqa
+
+    fps = Genre(genre='First Person Shooter')
+    rpg = Genre(genre='Role Playing Game')
+    mmo = Genre(genre='Massive Multiplayer Online')
+
+    third_person = Feature(feature='Third Person Camera')
+    looter_shooter = Feature(feature='Looter Shooter')
+    casual = Feature(feature='Filthy Casual')
+
     db.session.add(ian)
     db.session.add(javier)
     db.session.add(dean)
     db.session.add(angela)
     db.session.add(soonmi)
     db.session.add(alissa)
+
+    db.session.add(order1)
+    db.session.add(order2)
+    db.session.add(order3)
+
+    db.session.add(game1)
+    db.session.add(game2)
+
+    db.session.add(review1)
+    db.session.add(review2)
+
+    db.session.add(fps)
+    db.session.add(rpg)
+    db.session.add(mmo)
+
+    db.session.add(casual)
+    db.session.add(looter_shooter)
+    db.session.add(casual)
 
     db.session.commit()
