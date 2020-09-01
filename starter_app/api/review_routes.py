aebe3ebce56548c6d926b0d6faef754031628d32
Austin Burger <aburger91@gmail.com>
2020-09-01 15:32:53 -0500

    Add routes for orders. Add post/delete routes for users cart items, reviews, and orders

--- starter_app/api/review_routes.py
+++ starter_app/api/review_routes.py
@@ -1,16 +1,25 @@
-from flask import Blueprint, jsonify
-from starter_app.models import Review
+from flask import Blueprint, jsonify, request
+from starter_app.models import db, Review
 
 review_routes = Blueprint('reviews', __name__)
 
 
-@review_routes.route('/')
+@review_routes.route('/', methods=['GET', 'POST'])
 def index():
-    res = Review.query.all()
-    return {'reviews': [review.to_dict() for review in res]}
+    if request.method == 'POST':
+        form = request.form
+        review = Review(user_id=form['user_id'], game_id=form['game_id'], title=form['title'], body=form['body'], star_rating=form['star_rating'])  # noqa
+        db.session.add(review)
+        db.session.commit()
+        return {'review': review.to_dict()}
+    else:
+        res = Review.query.all()
+        return {'reviews': [review.to_dict() for review in res]}
 
 
-@review_routes.route('/<id>')
+@review_routes.route('/<id>', methods=['GET', 'DELETE'])
 def review(id):
     res = Review.query.get(id)
+    if request.method == 'DELETE':
+        db.session.delete(res)
     return res.to_dict()
