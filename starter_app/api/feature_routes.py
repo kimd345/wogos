6662e5b285ea74da8f33f92d3587855730b67b3c
Austin Burger <aburger91@gmail.com>
2020-09-01 18:14:41 -0500

    add feature routes

---
+++ starter_app/api/feature_routes.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, jsonify
+from starter_app.models import Feature
+
+feature_routes = Blueprint('features', __name__)
+
+
+@feature_routes.route('/')
+def index():
+    res = Feature.query.all()
+    return {'features': [feature.to_dict() for feature in res]}
+
+
+@feature_routes.route('/<id>')
+def feature(id):
+    res = Feature.query.get(id)
+    return res.to_dict()
